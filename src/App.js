bb74a47f0bd2d4a361cf26874557e2821f00b87c
nic0mp <nico.mp215@gmail.com>
2021-12-10 19:39:26 -0600

    starter files and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import { Header } from "./components/Header";
+import './App.css';
+
 function App() {
     return (
         <div >
-            Hello World
+           <Header/>
+           <div className="container">
+               <Balance />
+           </div>
         </div>
     );
 }
