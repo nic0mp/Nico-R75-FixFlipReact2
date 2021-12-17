6eb6203cdfe1922a8590602e9abea539f36c369c
nic0mp <nico.mp215@gmail.com>
2021-12-16 20:46:20 -0600

    final update for deployment to firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAn2alqpjPJCKuXLByN3gjJ4Taofz_vji4",
+    authDomain: "expense-trax.firebaseapp.com",
+    projectId: "expense-trax",
+    storageBucket: "expense-trax.appspot.com",
+    messagingSenderId: "993992047099",
+    appId: "1:993992047099:web:3b3bd00d4ac3fd7312735e"
+  };
